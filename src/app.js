55c51cd8290c78460eab501ec1d79e7b4fc32e1f
jing <jing@caravan.coop>
2023-02-05 00:07:21 -0500

    file handling and routing

--- src/app.js
+++ src/app.js
@@ -8,6 +8,12 @@ const morgan = require('morgan');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf-8');
+const accounts = JSON.parse(accountData);
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf-8');
+const users = JSON.parse(userData);
+
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -16,7 +22,22 @@ app.use(morgan('tiny'));
 
 
 app.get('/', (req, res) => {
-	res.render('index', {title: 'Index'});
+	res.render('index', {
+		title: 'Account Summary',
+		accounts,
+	});
+})
+app.get('/savings', (req, res) => {
+	res.render('account', {account: accounts.savings});
+})
+app.get('/checking', (req, res) => {
+	res.render('account', {account: accounts.checking});
+})
+app.get('/credit', (req, res) => {
+	res.render('account', {account: accounts.credit});
+})
+app.get('/profile', (req, res) => {
+	res.render('profile', {user: users[0]});
 })
 
 app.listen(PORT, () => debug(`Listerning on port ${chalk.green(PORT)}`));
