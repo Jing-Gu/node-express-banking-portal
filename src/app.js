d170844f255fb95d22be8742123df7b606fa8a90
jing <jing@caravan.coop>
2023-02-04 19:55:54 -0500

    add chalk, debug, morgan

--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('tiny'));
 
 
 app.get('/', (req, res) => {
 	res.render('index', {title: 'Index'});
 })
 
-app.listen(3000, () => console.log('PS project running on port 3000'));
+app.listen(PORT, () => debug(`Listerning on port ${chalk.green(PORT)}`));
