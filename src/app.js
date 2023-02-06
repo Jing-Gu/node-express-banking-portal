a59f171f3f827031a06db0968b1d0cf5b04947f2
jing <jing@caravan.coop>
2023-02-06 01:00:30 -0500

    refactor - routes

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 
 const {accounts, users, writeJSON} = require('./data');
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -18,6 +20,8 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('tiny'));
 app.use(express.urlencoded({extended: true}));
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 
 app.get('/', (req, res) => {
@@ -26,37 +30,10 @@ app.get('/', (req, res) => {
 		accounts,
 	});
 })
-app.get('/savings', (req, res) => {
-	res.render('account', {account: accounts.savings});
-})
-app.get('/checking', (req, res) => {
-	res.render('account', {account: accounts.checking});
-})
-app.get('/credit', (req, res) => {
-	res.render('account', {account: accounts.credit});
-})
+
 app.get('/profile', (req, res) => {
 	res.render('profile', {user: users[0]});
 })
-app.get('/transfer', (req, res) => {
-	res.render('transfer');
-})
-app.post('/transfer', (req, res) => {
-	accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
-	accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
-	writeJSON();
-	res.render('transfer', {message: 'Transfer Completed'});
-})
 
-app.get('/payment', (req, res) => {
-	res.render('payment', {account: accounts.credit});
-})
-
-app.post('/payment', (req, res) => {
-	accounts.credit.balance -= req.body.amount;
-	accounts.credit.available += parseInt(req.body.amount, 10);
-	writeJSON();
-	res.render('payment', {message: 'Payment successful', account: accounts.credit});
-})
 
 app.listen(PORT, () => debug(`Listerning on port ${chalk.green(PORT)}`));
